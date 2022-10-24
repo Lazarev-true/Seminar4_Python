a2137c689a4e508c7abddcd426f46f3f06dd823f
Konstantin <klik@mail.ru>
2022-10-24 21:01:47 +0300

    С формулой Бейли-Борвейна-Плуфа

--- Task1.py
+++ Task1.py
@@ -16,6 +16,11 @@ d2 = float(d1) + 1
 #     j += 1
 #     d1 *= 10
 
+n = j
+
+Pi_0 = sum(1/16**x*(4/(8*x + 1) - 2/(8*x + 4) - 1/(8*x + 5) - 1/(8*x + 6)) for x in range(n))
+print(f'при d = {d1}, π = {D(str(Pi_0)).quantize(D(str(d2)), decimal.ROUND_FLOOR)}')
+
 Pi_1 = (2 * math.acos( 0.0 ))
 print(f'при d = {d1}, π = {round(Pi_1, j)}')
 
